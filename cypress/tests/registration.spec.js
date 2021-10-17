c2f4c1cebe33029940623322ae658f1694a75b97
Lev <levz0r@gmail.com>
2021-10-17 23:05:06 +0300

    Temp. removed email flow.

--- cypress/tests/registration.spec.js
+++ cypress/tests/registration.spec.js
@@ -34,58 +34,58 @@ function generate_password() {
 }
 
 describe("Registration process", function () {
-  it("should be able to sign-in right after successful registration", function () {
-    // Sign-in screen
-    cy.visit("/signin");
-    cy.getBySel("signup").click();
+  // it("should be able to sign-in right after successful registration", function () {
+  //   // Sign-in screen
+  //   cy.visit("/signin");
+  //   cy.getBySel("signup").click();
 
-    const firstname = generate_name();
-    const lastname = generate_name();
-    const username = generate_username();
-    const password = generate_password();
-    const email = `lev+${generate_number()}@*****`;
-    const bankname = generate_username("capital");
-    const routingnumber = generate_number();
-    const accountnumber = generate_number();
+  //   const firstname = generate_name();
+  //   const lastname = generate_name();
+  //   const username = generate_username();
+  //   const password = generate_password();
+  //   const email = `lev+${generate_number()}@*****`;
+  //   const bankname = generate_username("capital");
+  //   const routingnumber = generate_number();
+  //   const accountnumber = generate_number();
 
-    Cypress.log({ name: "GENERATED", message: `First name: ${firstname}` });
-    Cypress.log({ name: "GENERATED", message: `Last name: ${lastname}` });
-    Cypress.log({ name: "GENERATED", message: `Username: ${username}` });
-    Cypress.log({ name: "GENERATED", message: `Password: ${password}` });
-    Cypress.log({ name: "GENERATED", message: `Email: ${email}` });
-    Cypress.log({ name: "GENERATED", message: `Bank name: ${bankname}` });
-    Cypress.log({ name: "GENERATED", message: `Routing number: ${routingnumber}` });
-    Cypress.log({ name: "GENERATED", message: `Account number: ${accountnumber}` });
+  //   Cypress.log({ name: "GENERATED", message: `First name: ${firstname}` });
+  //   Cypress.log({ name: "GENERATED", message: `Last name: ${lastname}` });
+  //   Cypress.log({ name: "GENERATED", message: `Username: ${username}` });
+  //   Cypress.log({ name: "GENERATED", message: `Password: ${password}` });
+  //   Cypress.log({ name: "GENERATED", message: `Email: ${email}` });
+  //   Cypress.log({ name: "GENERATED", message: `Bank name: ${bankname}` });
+  //   Cypress.log({ name: "GENERATED", message: `Routing number: ${routingnumber}` });
+  //   Cypress.log({ name: "GENERATED", message: `Account number: ${accountnumber}` });
 
-    // Sign-up screen
-    cy.getBySel("signup-first-name").type(firstname);
-    cy.getBySel("signup-last-name").type(lastname);
-    cy.getBySel("signup-email").type(email);
-    cy.getBySel("signup-username").type(username);
-    cy.getBySel("signup-password").type(password);
-    cy.getBySel("signup-confirmPassword").type(password);
-    cy.getBySel("signup-submit").click();
+  //   // Sign-up screen
+  //   cy.getBySel("signup-first-name").type(firstname);
+  //   cy.getBySel("signup-last-name").type(lastname);
+  //   cy.getBySel("signup-email").type(email);
+  //   cy.getBySel("signup-username").type(username);
+  //   cy.getBySel("signup-password").type(password);
+  //   cy.getBySel("signup-confirmPassword").type(password);
+  //   cy.getBySel("signup-submit").click();
 
-    // Sign-in screen
-    cy.login(username, password);
+  //   // Sign-in screen
+  //   cy.login(username, password);
 
-    // Main screen
-    cy.location().its("pathname").should("eq", "/"); // Assertion #1: We are now at the main page.
-    cy.getBySel("user-onboarding-dialog").should("be.visible"); // Assertion #2: Onboarding dialog is shown.
+  //   // Main screen
+  //   cy.location().its("pathname").should("eq", "/"); // Assertion #1: We are now at the main page.
+  //   cy.getBySel("user-onboarding-dialog").should("be.visible"); // Assertion #2: Onboarding dialog is shown.
 
-    // Onboarding dialog
-    cy.getBySel("user-onboarding-next").click();
+  //   // Onboarding dialog
+  //   cy.getBySel("user-onboarding-next").click();
 
-    cy.getBySel("bankaccount-bankName-input").type(bankname);
-    cy.getBySel("bankaccount-routingNumber-input").type(routingnumber);
-    cy.getBySel("bankaccount-accountNumber-input").type(accountnumber);
+  //   cy.getBySel("bankaccount-bankName-input").type(bankname);
+  //   cy.getBySel("bankaccount-routingNumber-input").type(routingnumber);
+  //   cy.getBySel("bankaccount-accountNumber-input").type(accountnumber);
 
-    cy.getBySel("bankaccount-submit").click();
-    cy.getBySel("user-onboarding-next").click();
+  //   cy.getBySel("bankaccount-submit").click();
+  //   cy.getBySel("user-onboarding-next").click();
 
-    // Main screen
-    cy.get("user-onboarding-dialog").should("not.exist"); // Assertion #3: Onboarding process is over. The dialog has been removed from the DOM.
-  });
+  //   // Main screen
+  //   cy.get("user-onboarding-dialog").should("not.exist"); // Assertion #3: Onboarding process is over. The dialog has been removed from the DOM.
+  // });
 
   it("should send an email with a correct activation token", function () {
     cy.visit("/signin");
